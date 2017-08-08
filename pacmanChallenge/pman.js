e0e572e60db0310ac8dfa76a457d60b2d1b3c43f
RedHenDev <redsheepdev@gmail.com>
2017-08-08 12:53:28 +0100

    I've forgotten everything :)

---
+++ pacmanChallenge/pman.js
@@ -0,0 +1,24 @@
+
+
+function setup(){
+    
+}
+
+
+function draw(){
+    
+    
+}
+
+
+
+class Pacan {
+    constructor(){
+        this.loc = new p5.Vector(width/2, height/2);
+        
+        this.
+        
+    }
+    
+    
+}
