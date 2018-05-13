ee54b009e03d91b7d6e9fafdc39807835a9e08ea
RedHenDev <redsheepdev@gmail.com>
2018-05-13 10:59:35 +0100

    3D co-ords working
    Yassss

--- 3D_tutorial/3D_tut_2.js
+++ 3D_tutorial/3D_tut_2.js
@@ -6,14 +6,6 @@ let epZ = 0;
 
 let epR = 0;
 
-let theta = 0;
-let phi = 0;
-
-// Vector to moon.
-let v;
-// Vector to moon moon.
-let vM;
-
 let rhTex;
 
 function setup(){
@@ -25,12 +17,6 @@ function setup(){
     
     rhTex = loadImage("RedHenIconAlpha512512.png");
     
-    // Random vectors for position of
-    // moon and moon-moon.
-    v = p5.Vector.random3D();
-    v.mult(300);
-    vM = p5.Vector.random3D();
-    vM.mult(62);
 }
 
 
@@ -39,84 +25,30 @@ function draw(){
     
     background(201,32,12);
     
-    pointLight( 255,255,255,
-                mouseX - width/2,
-                mouseY - height/2,
-                300);
     
-//    pointLight( 255,255,255,
-//                0,
-//                0,
-//                0);
-    
-    epR += 2;
+    epR += 0.1;
     epZ = map(mouseY, 0, height, 0, -100);
     
+    
     renderEggPlanet();
-}
-
-function renderMooon(){
-    
-    push();
-    
-    ambientMaterial(255);
-    
-//    let v2 = createVector(1,0,1);
-//    let p = v.cross(v2);
-//    rotate(radians(frameCount*2), p);
-    
-    let r = 300;
-    
-    theta = Math.atan2(v.y,v.x);
-    //Restricts -180,180
-    phi = 
-    Math.acos(v.z/Math.sqrt(
-        v.x*v.x+v.y*v.y+v.z*v.z));
-    //Restricts 0,180
-    
-    rotateZ(theta);
-    rotateY(phi);
-    
-    translate(v.x, v.y, v.z);
-    
-    sphere(42,80,40);
-    
-        //renderMooon2();
-    
-    pop(); 
     
 }
 
-function renderMooon2(){
-    
-    push();
-    
-    specularMaterial(255,0,0);
-    
-    let v3 = createVector(1,0,1);
-    let pp = vM.cross(v3);
-    rotate(-radians(frameCount*5), pp);
-    
-    translate(vM.x, vM.y, vM.z);
-    
-    sphere(12,80,40);
-    
-    pop();
-}
 
 function renderEggPlanet(){
     
-    push();
-    
-    //texture(rhTex);
-    specularMaterial(0,255,0);
-    //ambientMaterial(0,255,0);
-    
+    texture(rhTex);
     translate(epX, epY, epZ);
-    //rotateY(radians(epR/2));
-    sphere(200,240,160);
-        
-        renderMooon();
+    rotateY(radians(epR));
+    sphere(200);
     
-    pop();  
 }
+
+
+
+
+
+
+
+
+
---
+++ 3D_tutorial/orbit3D.js
@@ -0,0 +1,123 @@
+
+
+let epX = 0;
+let epY = 0;
+let epZ = 0;
+
+let epR = 0;
+
+let theta = 0;
+let phi = 0;
+let radius = 0;
+
+// Vector to moon.
+let v;
+// Vector to moon moon.
+let vM;
+
+let rhTex;
+
+function setup(){
+    
+    createCanvas(windowWidth,
+                windowHeight, WEBGL);
+    
+    background(201,32,12);
+    
+    rhTex = loadImage("RedHenIconAlpha512512.png");
+    
+    // Random vectors for position of
+    // moon and moon-moon.
+    v = p5.Vector.random3D();
+    v.mult(300);
+    v = createVector(300,0,0);
+    
+    radius = Math.sqrt
+    (v.x * v.x + v.y * v.y + v.z * v.z);
+    radius = 300;
+    theta  = Math.acos(v.z / radius);
+    phi    = atan2(v.y, v.x);
+
+    
+    vM = p5.Vector.random3D();
+    vM.mult(62);
+}
+
+
+
+function draw(){
+    
+    background(201,32,12);
+    
+    pointLight( 255,255,255,
+                mouseX - width/2,
+                mouseY - height/2,
+                300);
+    
+//    pointLight( 255,255,255,
+//                0,
+//                0,
+//                0);
+    
+    epR += 2;
+    epZ = map(mouseY, 0, height, 0, -1000);
+    
+    renderEggPlanet();
+}
+
+function renderMooon(){
+    
+    push();
+    
+    ambientMaterial(255);
+    
+    rotateZ(radians(20));
+    
+    theta += 0.01;
+    v.x = radius * Math.sin(theta) * cos(phi);
+    v.y = radius * Math.sin(theta) * sin(phi);
+    v.z = radius * Math.cos(theta);
+    
+    translate(v.x, v.y, v.z);
+    
+    sphere(42,80,40);
+    
+        //renderMooon2();
+    
+    pop(); 
+    
+}
+
+function renderMooon2(){
+    
+    push();
+    
+    specularMaterial(255,0,0);
+    
+    let v3 = createVector(1,0,1);
+    let pp = vM.cross(v3);
+    rotate(-radians(frameCount*5), pp);
+    
+    translate(vM.x, vM.y, vM.z);
+    
+    sphere(12,80,40);
+    
+    pop();
+}
+
+function renderEggPlanet(){
+    
+    push();
+    
+    //texture(rhTex);
+    specularMaterial(0,255,0);
+    //ambientMaterial(0,255,0);
+    
+    translate(epX, epY, epZ);
+    //rotateY(radians(epR/2));
+    sphere(200,240,160);
+        
+        renderMooon();
+    
+    pop();  
+}
